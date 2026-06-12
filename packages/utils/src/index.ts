f55d40fbd361abeb450dee55350e3acec4b133f8
EternalHeart <hao131462@qq.com>
2026-06-12 19:17:52 +0800

    chore: 更新共享包导出
    - 导出 AI 聊天相关类型和工具

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,35 +1,43 @@
 // 时间相关工具
-export * from './time'
+export * from './time';
 
 // 请求相关工具
-export * from './request'
-export * from './api-client'
+export * from './request';
+export * from './api-client';
 
 // 字符串处理工具
-export * from './string'
+export * from './string';
 
 // 配置相关工具
-export * from './config'
+export * from './config';
 
 // 博客API服务
-export * from './blog-api'
+export * from './blog-api';
+
+// AI 对话工具
+export * from './ai-chat';
 
 // 类型导出
-export type { TimeFormatOptions } from './time'
-export type { RequestConfig, RequestOptions, ApiResponse, ApiError } from './request'
-export type { TruncateOptions } from './string'
-export type { AppConfig } from './config'
-export type { 
-  Post, 
-  CreatePostDto, 
-  UpdatePostDto, 
-  PostListResponse, 
-  LoginDto, 
+export type { TimeFormatOptions } from './time';
+export type {
+  RequestConfig,
+  RequestOptions,
+  ApiResponse,
+  ApiError,
+} from './request';
+export type { TruncateOptions } from './string';
+export type { AppConfig } from './config';
+export type {
+  Post,
+  CreatePostDto,
+  UpdatePostDto,
+  PostListResponse,
+  LoginDto,
   LoginResponse,
   Category,
   Tag,
   CreateCategoryDto,
   UpdateCategoryDto,
   CreateTagDto,
-  UpdateTagDto
-} from './blog-api'
+  UpdateTagDto,
+} from './blog-api';
